814a6ec64240a643c410e8704167843bec29ed16
Labann <labanmusen32@gmail.com>
2025-10-22 17:28:19 +0300

    feat: replicated refrence nav-bar

--- kitesurfing_25/src/app/components/Navbar.tsx
+++ kitesurfing_25/src/app/components/Navbar.tsx
@@ -1,96 +1,146 @@
 'use client';
-import React from 'react';
-import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react';
+import React, { useEffect, useRef, useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-import { RiMenu3Fill } from 'react-icons/ri';
-import { AiOutlineClose } from 'react-icons/ai';
+import { IoMdClose, IoMdMenu } from 'react-icons/io';
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const navRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (navRef.current && !navRef.current.contains(event.target as Node)) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
   return (
-    <nav className={`bg-primary relative p-4`}>
-      <div className={`mx-auto flex max-w-7xl items-center justify-between`}>
-        <Link href={`/`}>
-          <Image src="/logo_dark.png" width="90" height="90" alt="logo-img" />
-        </Link>
-
-        <ul className={'hidden items-center space-x-4 md:flex'}>
-          <li className={'hover:bg-accent text-default rounded p-2'}>
-            <Link href={'/'} className={'text-default'}>
-              Home
-            </Link>
-          </li>
-          <li className={'hover:bg-accent text-default rounded p-2'}>
-            <a href="#lesson">price</a>
-          </li>
-          <li className={'hover:bg-accent text-default rounded p-2'}>
-            <a href="#about">About</a>
-          </li>
-          <li className={'hover:bg-accent text-default rounded p-2'}>
-            <a href="#location">location</a>
-          </li>
-          <li className={'hover:bg-accent text-default rounded p-2'}>
-            <a href="#contacts">contacts</a>
-          </li>
-        </ul>
-
-        <div className={'md:hidden'}>
-          <Menu>
-            {({ open }) => (
-              <>
-                <MenuButton className="text-default cursor-pointer">
-                  {open ? (
-                    <AiOutlineClose size={`1.5em`} />
-                  ) : (
-                    <RiMenu3Fill size={`1.5em`} />
-                  )}
-                </MenuButton>
-                <MenuItems className="bg-primary/10 absolute top-22 left-0 flex w-screen flex-col space-y-3 p-3">
-                  <MenuItem>
-                    <Link
-                      href="/"
-                      className="data-focus:bg-accent data-focus:text-default text-secondary block rounded-md p-2 text-center font-bold"
-                    >
-                      Home
-                    </Link>
-                  </MenuItem>
-                  <MenuItem>
-                    <a
-                      href="#price"
-                      className="data-focus:bg-accent data-focus:text-default text-secondary block rounded-md p-2 text-center font-bold"
-                    >
-                      price
-                    </a>
-                  </MenuItem>
-                  <MenuItem>
-                    <a
-                      href="#about"
-                      className="data-focus:bg-accent data-focus:text-default text-secondary block rounded-md p-2 text-center font-bold"
-                    >
-                      About
-                    </a>
-                  </MenuItem>
-                  <MenuItem>
-                    <a
-                      href="#location"
-                      className="data-focus:bg-accent data-focus:text-default text-secondary block rounded-md p-2 text-center font-bold"
-                    >
-                      location
-                    </a>
-                  </MenuItem>
-                  <MenuItem>
-                    <a
-                      href="#contacts"
-                      className="data-focus:bg-accent data-focus:text-default text-secondary block rounded-md p-2 text-center font-bold"
-                    >
-                      contacts
-                    </a>
-                  </MenuItem>
-                </MenuItems>
-              </>
-            )}
-          </Menu>
+    <nav ref={navRef}>
+      <div className={`bg-primary relative px-6 py-4`}>
+        <div className={`mx-auto flex items-center justify-between`}>
+          <Link href={`/`}>
+            <Image src="/logo_dark.png" width="90" height="90" alt="logo-img" />
+          </Link>
+          {isOpen ? (
+            <IoMdClose
+              size={`1.5em`}
+              className="cursor-pointer sm:hidden"
+              onClick={() => setIsOpen(!isOpen)}
+            />
+          ) : (
+            <IoMdMenu
+              size={`1.5em`}
+              className="cursor-pointer sm:hidden"
+              onClick={() => setIsOpen(!isOpen)}
+            />
+          )}
+          <ul
+            className={
+              'semi-bold hidden items-center space-x-8 text-sm sm:flex'
+            }
+          >
+            <li
+              className={
+                'hover:bg-accent text-default cursor-pointer rounded p-2'
+              }
+            >
+              <Link href={'/'} className={'text-default'}>
+                Home
+              </Link>
+            </li>
+            <li
+              className={
+                'hover:bg-accent text-default cursor-pointer rounded p-2'
+              }
+            >
+              <a href="#lesson">price</a>
+            </li>
+            <li
+              className={
+                'hover:bg-accent text-default cursor-pointer rounded p-2'
+              }
+            >
+              <a href="#about">About</a>
+            </li>
+            <li
+              className={
+                'hover:bg-accent text-default cursor-pointer rounded p-2'
+              }
+            >
+              <a href="#location">location</a>
+            </li>
+            <li
+              className={
+                'hover:bg-accent text-default cursor-pointer rounded p-2'
+              }
+            >
+              <a href="#contacts">contacts</a>
+            </li>
+          </ul>
         </div>
       </div>
+      {isOpen && (
+        <div
+          className={
+            'bg-primary fixed flex w-full flex-col space-y-2 p-4 md:hidden'
+          }
+        >
+          <Link
+            href={'/'}
+            onClick={() => setIsOpen(!isOpen)}
+            className={
+              'hover:bg-accent text-default cursor-pointer rounded p-2'
+            }
+          >
+            Home
+          </Link>
+
+          <a
+            href="#lesson"
+            onClick={() => setIsOpen(!isOpen)}
+            className={
+              'hover:bg-accent text-default cursor-pointer rounded p-2'
+            }
+          >
+            price
+          </a>
+
+          <a
+            href="#about"
+            onClick={() => setIsOpen(!isOpen)}
+            className={
+              'hover:bg-accent text-default cursor-pointer rounded p-2'
+            }
+          >
+            About
+          </a>
+
+          <a
+            href="#location"
+            onClick={() => setIsOpen(!isOpen)}
+            className={
+              'hover:bg-accent text-default cursor-pointer rounded p-2'
+            }
+          >
+            location
+          </a>
+
+          <a
+            href="#contacts"
+            onClick={() => setIsOpen(!isOpen)}
+            className={
+              'hover:bg-accent text-default cursor-pointer rounded p-2'
+            }
+          >
+            contacts
+          </a>
+        </div>
+      )}
     </nav>
   );
 };
