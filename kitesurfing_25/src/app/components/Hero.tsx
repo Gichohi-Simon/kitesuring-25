23ffcaf8392bc0dc75cc37ffb38a4133d1227055
Labann <labanmusen32@gmail.com>
2025-10-23 07:37:52 +0300

    hero section done

--- kitesurfing_25/src/app/components/Hero.tsx
+++ kitesurfing_25/src/app/components/Hero.tsx
@@ -1,10 +1,13 @@
 'use client';
 import React from 'react';
-
+import Image from 'next/image';
 const Hero = () => {
   return (
-    <div id="hero" className="min-h-screen">
-      Hero
+    <div
+      id="hero"
+      className="flex min-h-[90vh] items-center justify-center bg-[url('/ocean.jpg')] bg-cover bg-center"
+    >
+      <Image src={'/logo_light.png'} alt="hero-img" width={300} height={300} />
     </div>
   );
 };
