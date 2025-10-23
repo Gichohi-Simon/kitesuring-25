e74786f6658d418f4651ea9fe80ba6335b37b5b2
Laban Magotsi <72252450+Labann@users.noreply.github.com>
2025-10-23 17:56:51 +0300

    lessons section done (#5)
    * lessons section done
    
    * seperated card_data to a different file

--- kitesurfing_25/src/app/components/Lesson.tsx
+++ kitesurfing_25/src/app/components/Lesson.tsx
@@ -1,10 +1,25 @@
 'use client';
 import React from 'react';
+import Card from './Card';
+import cardData from '../utils/cardData';
 
 const Lesson = () => {
   return (
-    <div id="lesson" className="min-h-screen">
-      Lesson
+    <div className="p-4 py-10">
+      <div className="text-center text-lg">
+        <h3 className="text-primary text-xl font-bold">Lesson pricing</h3>
+        <p>All lesson include equipment plus instructions</p>
+        <p>(recommended: 9 hours minimum for complete beginners)</p>
+        <p>cash only in euros, usd, kes, gbp</p>
+      </div>
+      <div
+        id="lesson"
+        className="grid gap-7 py-10 md:grid-cols-2 lg:grid-cols-3"
+      >
+        {cardData.map((data) => (
+          <Card key={data.id} data={data} />
+        ))}
+      </div>
     </div>
   );
 };
