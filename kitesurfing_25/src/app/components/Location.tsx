38b08a5727777bfa902bc2056ecc7175f71267d4
Labann <labanmusen32@gmail.com>
2025-11-04 08:02:49 +0300

    corrcetd casiing of text

--- kitesurfing_25/src/app/components/Location.tsx
+++ kitesurfing_25/src/app/components/Location.tsx
@@ -6,7 +6,7 @@ const Location = () => {
   return (
     <div id="location" className="min-h-screen py-10">
       <h3 className="text-primary text-center text-lg font-extrabold">
-        Come Surf with Us
+        Come surf with us
       </h3>
       <iframe
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5165.378077284496!2d39.55654197665545!3d-4.3621172126795775!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x18404c027c766b13%3A0xd8dba60878c0f449!2sKenya%20Kitesurfing%20School!5e0!3m2!1sen!2ske!4v1596204395022!5m2!1sen!2ske"
